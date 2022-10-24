799503b0ad0ed645c65320f7abc5c43cab59f32d
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-10-24 09:21:44 -0300

    Created 'show_menu' function

--- loto_sortRS/src/main.rs
+++ loto_sortRS/src/main.rs
@@ -1,3 +1,18 @@
+fn show_menu() {
+    println!("======= lotoSort ========");
+    println!("_________________________");
+    println!("| - Jogos disponíveis - |");
+    println!("|-----------------------|");
+    println!("| 1 - Quina             |");
+    println!("| 2 - Mega-Sena         |");
+    println!("| 3 - LotoMania         |");
+    println!("| 4 - LotoFácil         |");
+    println!("|- - - - - - - - - - - -|");
+    println!("| 5 - Sair              |");
+    println!("|-----------------------|");
+    println!("Digite o número do tipo de sorteio: \n> ");
+}
+
 fn main() {
-    println!("Hello, world!");
+    show_menu();
 }
