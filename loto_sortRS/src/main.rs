403c035ec2f34a9f0bda39e7775c7b3bdda4cc7c
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-10-24 12:16:39 -0300

    Added (test) functions to generate the numbers of the drawings

--- loto_sortRS/src/main.rs
+++ loto_sortRS/src/main.rs
@@ -17,19 +17,46 @@ fn show_menu() {
     println!("Digite o número do tipo de sorteio: ");
 }
 
+fn quina() {
+    println!("Jogo quina");
+}
+
+fn sena() {
+    println!("Sena");
+}
+
+fn mania() {
+    println!("Mania");
+}
+
+fn facil() {
+    println!("Fácil");
+}
+
 fn main() {
-    show_menu();
+    loop {
+        show_menu();
 
-    let mut opcao = String::new();
+        let mut opcao = String::new();
 
-    io::stdin()
-        .read_line(&mut opcao)
-        .expect("Falha ao ler entrada");
+        io::stdin()
+            .read_line(&mut opcao)
+            .expect("Falha ao ler entrada");
 
-    let opcao: u32 = opcao
-        .trim()
-        .parse()
-        .expect("Por favor, digite apenas números");
+        let opcao: i32 = opcao
+            .trim()
+            .parse()
+            .expect("Por favor, digite apenas números");
 
-    println!("Opção escolhida: {}", opcao);
+        println!("Opção escolhida: {}", opcao);
+
+        match opcao {
+            1 => quina(),
+            2 => sena(),
+            3 => mania(),
+            4 => facil(),
+            5 => break,
+            _ => println!("Opção inválida!"),
+        }
+    }
 }
